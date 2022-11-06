02ad3ee83b332afcd7905a59702be7e40f55b43b
liuguolin <liuguolin@nowcoder.com>
2022-11-06 12:07:45 +0800

    feat: ui升级/添加i8n/组件抽离/移动端适配

--- js/chunk-2d22d746.05109826.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,s){"use strict";s.r(t);var u=function(){var n=this;n._self._c;return n._m(0)},a=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],i=s("2877"),c={},e=Object(i["a"])(c,u,a,!1,null,null,null);t["default"]=e.exports}}]);
-//# sourceMappingURL=chunk-2d22d746.05109826.js.map
